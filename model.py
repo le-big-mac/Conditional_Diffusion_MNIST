934d449681bf375fcd2d3917267d5721ec7f2cf7
le-big-mac <charles.london@cs.ox.ac.uk>
2024-04-04 17:19:08 +0100

    add param sampling functionality

--- model.py
+++ model.py
@@ -224,7 +224,7 @@ class DDPM(nn.Module):
         self.drop_prob = drop_prob
         self.loss_mse = nn.MSELoss()
 
-    def forward(self, x, c):
+    def forward(self, x, c, num_param_samples=10):
         """
         this method is used in training, so samples t and noise randomly
         """
@@ -242,18 +242,21 @@ class DDPM(nn.Module):
         context_mask = torch.bernoulli(torch.zeros_like(c)+self.drop_prob).to(self.device)
 
         # return MSE between added noise, and our predicted noise
-        return self.loss_mse(noise, self.nn_model(x_t, c, _ts / self.n_T, context_mask))
+        x_t = x_t.repeat(num_param_samples, 1, 1, 1)
+        param_sample_fn = torch.vmap(self.nn_model, in_dims=(0, None, None, None), randomness='different')
+        out = param_sample_fn(x_t, c, _ts / self.n_T, context_mask).mean(dim=0)
+        return self.loss_mse(noise, out)
 
-    def sample(self, n_sample, size, device, guide_w = 0.0):
+    def sample(self, num_noise_samples, num_param_samples, num_classes, size, device, guide_w = 0.0):
         # we follow the guidance sampling scheme described in 'Classifier-Free Diffusion Guidance'
         # to make the fwd passes efficient, we concat two versions of the dataset,
         # one with context_mask=0 and the other context_mask=1
         # we then mix the outputs with the guidance scale, w
         # where w>0 means more guidance
 
-        x_i = torch.randn(n_sample, *size).to(device)  # x_T ~ N(0, 1), sample initial noise
-        c_i = torch.arange(0,10).to(device) # context for us just cycles throught the mnist labels
-        c_i = c_i.repeat(int(n_sample/c_i.shape[0]))
+        x_i = torch.randn(num_noise_samples, *size).to(device)  # x_T ~ N(0, 1), sample initial noise
+        c_i = torch.arange(0,num_classes).to(device) # context for us just cycles throught the mnist labels
+        c_i = c_i.repeat(int(num_noise_samples/c_i.shape[0]))
 
         # don't drop context at test time
         context_mask = torch.zeros_like(c_i).to(device)
@@ -261,27 +264,31 @@ class DDPM(nn.Module):
         # double the batch
         c_i = c_i.repeat(2)
         context_mask = context_mask.repeat(2)
-        context_mask[n_sample:] = 1. # makes second half of batch context free
+        context_mask[num_noise_samples:] = 1. # makes second half of batch context free
 
         x_i_store = [] # keep track of generated steps in case want to plot something
         print()
         for i in range(self.n_T, 0, -1):
             print(f'sampling timestep {i}',end='\r')
             t_is = torch.tensor([i / self.n_T]).to(device)
-            t_is = t_is.repeat(n_sample,1,1,1)
+            t_is = t_is.repeat(num_noise_samples,1,1,1)
 
             # double batch
             x_i = x_i.repeat(2,1,1,1)
             t_is = t_is.repeat(2,1,1,1)
 
-            z = torch.randn(n_sample, *size).to(device) if i > 1 else 0
+            z = torch.randn(num_noise_samples, *size).to(device) if i > 1 else 0
 
             # split predictions and compute weighting
-            eps = self.nn_model(x_i, c_i, t_is, context_mask)
-            eps1 = eps[:n_sample]
-            eps2 = eps[n_sample:]
+            x_i = x_i.repeat(num_param_samples, 1, 1, 1, 1)
+            param_sample_fn = torch.vmap(self.nn_model, in_dims=(0, None, None, None), randomness='different')
+            eps = param_sample_fn(x_i, c_i, t_is, context_mask).mean(dim=0)
+
+            # eps = self.nn_model(x_i, c_i, t_is, context_mask)
+            eps1 = eps[:num_noise_samples]
+            eps2 = eps[num_noise_samples:]
             eps = (1+guide_w)*eps1 - guide_w*eps2
-            x_i = x_i[:n_sample]
+            x_i = x_i[:num_noise_samples]
             x_i = (
                 self.oneover_sqrta[i] * (x_i - eps * self.mab_over_sqrtmab[i])
                 + self.sqrt_beta_t[i] * z
