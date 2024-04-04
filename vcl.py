6ffa4f67ee376ce224ce896289b57cc47c76fa21
le-big-mac <charles.london@cs.ox.ac.uk>
2024-04-05 00:07:14 +0100

    no gif

--- vcl.py
+++ vcl.py
@@ -34,23 +34,27 @@ for i in range(n_classes):
 
 digit_datasets = get_split_MNIST()
 
-# MLE training
-nn_model = ContextUnet(1, n_feat, n_classes, mle=True)
-ddpm_mle = DDPM(nn_model, betas=(1e-4, 0.02), n_T=n_T, device=device, drop_prob=0.1)
-ddpm_mle.to(device)
-zero_loader = data.DataLoader(digit_datasets[0], batch_size=batch_size, shuffle=True, num_workers=0)
-optim = torch.optim.Adam(ddpm_mle.parameters(), lr=lrate)
-prior_mu, prior_logvar = stack_params(nn_model)
+if not mle_comp:
+    # MLE pretraining
+    nn_model = ContextUnet(1, n_feat, n_classes, mle=True)
+    ddpm_mle = DDPM(nn_model, betas=(1e-4, 0.02), n_T=n_T, device=device, drop_prob=0.1)
+    ddpm_mle.to(device)
+    zero_loader = data.DataLoader(digit_datasets[0], batch_size=batch_size, shuffle=True, num_workers=0)
+    optim = torch.optim.Adam(ddpm_mle.parameters(), lr=lrate)
+    prior_mu, prior_logvar = stack_params(nn_model)
+    for ep in range(n_epoch):
+        print(f"Epoch {ep}")
+        optim.param_groups[0]['lr'] = lrate*(1-ep/n_epoch)
+        train_epoch(ddpm_mle, zero_loader, optim, device)
 
-for ep in range(n_epoch):
-    print(f"Epoch {ep}")
-    optim.param_groups[0]['lr'] = lrate*(1-ep/n_epoch)
-    train_epoch(ddpm_mle, zero_loader, optim, device)
+    ddpm_mle.cpu()
+else:
+    prior_mu, prior_logvar = None, None
 
-ddpm_mle.cpu()
 nn_model = ContextUnet(1, n_feat, n_classes, mle=mle_comp)
 ddpm = DDPM(nn_model, betas=(1e-4, 0.02), n_T=n_T, device=device, drop_prob=0.1)
-ddpm.load_state_dict(ddpm_mle.state_dict())
+if not mle_comp:
+    ddpm.load_state_dict(ddpm_mle.state_dict())
 ddpm.to(device)
 
 for digit in range(n_classes):
@@ -61,7 +65,8 @@ for digit in range(n_classes):
         print(f"Epoch {ep}")
         optim.param_groups[0]['lr'] = lrate*(1-ep/n_epoch)
         x, c = train_epoch(ddpm, digit_loader, optim, device, prior_mu=prior_mu, prior_logvar=prior_logvar, mle=mle_comp)
-        save_gif = True if ep == n_epoch - 1 or ep%5 == 0 else False
+        # save_gif = True if ep == n_epoch - 1 or ep%5 == 0 else False
+        save_gif = False
         eval(ep, ddpm, digit+1, f"{save_dir}/{digit}", device, save_gif=save_gif)
 
     prior_mu, prior_logvar = stack_params(nn_model)
