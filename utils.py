5f267930c7a2a859cb2f4bf55baf5ff9bc7f57b7
le-big-mac <charles.london@cs.ox.ac.uk>
2024-04-07 04:00:59 +0100

    fix image saving

--- utils.py
+++ utils.py
@@ -70,14 +70,14 @@ def train_epoch(ddpm, dataloader, optim, device, num_param_samples=10, prior_mu=
 def eval(ep, ddpm, n_classes, save_dir, device, ws_test=[0.5, 2.0, 5.0], save_gif=False, num_eval_samples=10, save_name=None):
     ddpm.eval()
     if save_name is None:
-        save_name = f"image_ep{ep}_w{{w}}.png"
+        save_name = f"image_ep{ep}"
 
     n_noise_samples = 4 * n_classes
     for w_i, w in enumerate(ws_test):
         x_gen, x_gen_store = ddpm.sample(n_noise_samples, n_classes, (1, 28, 28), device, guide_w=w, num_param_samples=num_eval_samples)
         grid = make_grid(x_gen*-1 + 1, nrow=n_classes)
-        save_image(grid, save_dir + save_name)
-        print('saved image at ' + save_dir + save_name)
+        save_image(grid, f"{save_dir}/{save_name}.png")
+        print(f"saved image at {save_dir}/{save_name}_w{w}.png")
 
         # if save_gif:
         #     fig, axs = plt.subplots(nrows=int(n_noise_samples/n_classes), ncols=n_classes,sharex=True,sharey=True,figsize=(8,3))
